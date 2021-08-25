839d67e26d86dec260b8f4c3ec592daee85d39e3
Alex <ajlexmaritime@gmail.com>
2021-08-26 00:02:20 +0300

    2 task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React, { Component } from 'react';
+
+import ContactForm from './components/ContactForm/ContactForm';
+import ContactList from './components/ContactList/ContactList';
+import shortid from 'shortid';
+
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: ' ',
+  }
+
+
+  formSubmit = ({ name, number }) => {
+    const contact = {
+      id: shortid.generate(),
+      name,
+      number,
+    }
+
+    const equalContact = this.state.contacts.find(
+      c => c.name === contact.name
+    );
+
+    if (equalContact) return alert(`${name} is already in contacts`);
+    
+    this.setState(({contacts})=> ({
+        contacts: [contact, ...contacts],
+      }));
+    
+  }
+    
+  
+
+  filterContacts = () => {
+    const { contacts, filter } = this.state;
+    const toLowerCaseFilter = filter.toLowerCase();
+    return contacts.filter(contact => {
+      return contact.name.toLowerCase().includes(toLowerCaseFilter)
+    });
+  }
+
+  onDelete = id => {
+    const { contacts } = this.state;
+    this.setState({
+      contacts: contacts.filter(contact => contact.id !== id)
+    });
+  }
+  
+
+  render() {
+    // const { contacts } = this.state;
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.formSubmit} />
+        <h2>Contacts</h2>
+        <ContactList
+          contacts={ this.filterContacts()}
+          onDelete={ this.onDelete}
+        />
+        
+      </div >
+    )
+  }
 }
 
 export default App;
